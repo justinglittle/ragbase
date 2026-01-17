2e9c15ca462b74aadbf5c6e8a7936f43cb2247c5
justinglittle <justin.g.little@gmail.com>
2026-01-17 12:09:59 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
-import KnowledgeManagementApp from "./components/KnowledgeManagementApp";
-
 export default function App() {
-  return <KnowledgeManagementApp />;
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Knowledge Manager</h1>
+      <p>React mounted successfully.</p>
+    </div>
+  );
 }
